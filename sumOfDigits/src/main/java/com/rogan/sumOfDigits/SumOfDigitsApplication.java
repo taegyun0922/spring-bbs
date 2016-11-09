0f5210dfb45d5b1f9abd702ec5c2115a59f2fb52
rogan <taegyun0922@gmail.com>
2016-11-09 21:06:41 +0900

    main page

---
+++ sumOfDigits/src/main/java/com/rogan/sumOfDigits/SumOfDigitsApplication.java
@@ -0,0 +1,12 @@
+package com.rogan.sumOfDigits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumOfDigitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SumOfDigitsApplication.class, args);
+	}
+}
